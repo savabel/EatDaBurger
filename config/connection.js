b11fcb74c1584184dd5d042003bd8deac2c448d2
Abel JM SAVOEDA <60677888+savabel@users.noreply.github.com>
2020-05-25 20:07:06 -0400

    Adding moe files

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql2");
 
-var connection = mysql.createConnection({
+var connection;
+if (Process.env.JAWSDB_URL){
+  connection=mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+  connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
   password: "Passw@rd",
   database: "burgers_db"
 });
-
+  };
+  
 // Make connection.
 connection.connect(function(err) {
   if (err) {
